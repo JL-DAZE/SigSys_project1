f586ebc5332fcfa77e3a6c089af5b2cb7437e2a8
Chang Yan <cyan13@jhu.edu>
2021-03-23 18:49:19 +0800

    part 1 down

--- sonar.m
+++ sonar.m
@@ -5,8 +5,21 @@ plot(SonarPing,'-r');
 plot(SonarEcho,'-b');
 
 echoLength = length(SonarEcho);
-pingLength = length(SonarPing);
+T = length(SonarPing);
 
-Filtered = conv(fliplr(SonarPing), SonarEcho);
+Filtered = conv(SonarEcho, fliplr(SonarPing));
 figure(2);
 plot(Filtered, '-r');
+
+% calculate and show match result
+[value, index] = max(Filtered);
+result = index - T;
+figure(3)
+hold on
+plot(SonarPing,'-r');
+plot(SonarEcho,'-b');
+plot([result result],[min(SonarEcho) max(SonarEcho)],'-p')
+plot([result + T result + T],[min(SonarEcho) max(SonarEcho)],'-p')
+
+distance = result / 100 * 5000 / 2;
+fprintf("The distance is %d\n", distance);
